8f212287020ad2bd2ae49db2f57a1cd5824bf69b
willieWonkajr <wfjdbidub@gmail.com>
2023-05-28 03:47:06 -0400

    WizzMacs V0.2

--- custom-vars.el
+++ custom-vars.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(custom-safe-themes
+   '("991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "e963debb6066f1b4a6bdd24b2f4bf0ba4496c5ab7ecced9debfd4d1a512d94d1" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" default))
+ '(org-agenda-files '("~/Desktop/OrgTest/Scheduling.org"))
+ '(package-selected-packages
+   '(evil-org org-bullets org-bullet org-superstar treemacs corfu eglot eglog vterm rustic company treemode flycheck rust-mode lsp-mode orderless vertico marginalia doom-themes tao-theme helm doom-modeline counsel org evil))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 (setq inhibit-startup-message t) ; Removes starting page
 
-
 (tool-bar-mode -1);Determins if tool bar shows up
 (scroll-bar-mode -1)
 (menu-bar-mode 1)
@@ -8,12 +7,9 @@
 (global-display-line-numbers-mode 1);Activates number lines
 (setq display-line-numbers-type 'relative);Number lines are relative to the current line
 
-(load-theme 'modus-vivendi t) ; Loads theme
-
 (recentf-mode 1); loads recent files that you have edited
 
 (setq history-length 20) ;Sets the amount of recent files tracked
-(savehist-mode 1) ; Activates file history
 
 (save-place-mode 1) ; Saves and restores last location of file
 
@@ -29,11 +25,165 @@
 (setq use-dialog-box nil);Turns off graphical dialog box(less mouse clickey)
 
 (global-auto-revert-mode 1);If file has changed, auto loads changes
-
-
 (setq global-auto-revert-non-file-buffers t);;Same as above but for all buffers
 
-(add-to-list 'load-path "packages/evil.el")
-(require 'evil)
-(evil-mode 1)
+;; Initialize package sources
+(require 'package)
 
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+
+(use-package doom-themes
+  :ensure t
+  :config
+   (setq doom-themes-enable-bold t   ; if nil, bold is universally disabled
+         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+   (load-theme 'doom-acario-dark t))
+
+(use-package doom-modeline
+  :ensure t
+  :config
+  (doom-modeline-mode 1))
+
+(use-package nerd-icons
+ :custom
+;; The Nerd Font you want to use in GUI
+;; "Symbols Nerd Font Mono" is the default and is recommended
+;; but you can use any other Nerd Font if you want
+ (nerd-icons-font-family "Symbols Nerd Font Mono")
+)
+
+(use-package vterm
+  :ensure t
+  :commands vterm
+  :config
+  (setq vterm-max-scrollback 10000))
+
+(use-package org
+  :ensure t
+  :config
+  (org-mode 1))
+
+(setq org-hidden-keywords '(title))
+(setq org-startup-indented t)
+(setq org-startup-inline-images t)
+
+(use-package evil-org
+:ensure t
+:after (evil org)
+:config
+(add-hook 'org-mode-hook 'evil-org-mode)
+(add-hook 'evil-org-mode-hook
+          (lambda ()
+            (evil-org-set-key-theme '(navigation insert textobjects additional calendar))))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys))
+
+;;select languages for bable
+(org-babel-do-load-languages
+'org-babel-load-languages
+'((emacs-lisp . t)))
+
+(setq org-confirm-babel-evaluate nil);;Confirmation to execute code block
+
+(defun efs/org-babel-tangle-config()
+(when(string-equal (buffer-file-name)
+                   (expand-file-name "~/.emacs.d/WizzyMacs.org"))
+
+(let ((org-confirm-babel-evaluate nil))
+  (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
+
+(use-package org-tempo)
+(add-to-list 'org-structure-template-alist '("el". "src emacs-lisp"));;Autofill code blocks
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
+  (evil-mode 1))
+
+(use-package vertico
+:ensure t
+:config
+(vertico-mode 1))
+
+(use-package marginalia
+  :after vertico
+  :ensure t
+  :config
+  (marginalia-mode 1))
+
+(use-package savehist
+:config
+(savehist-mode))
+
+(use-package which-key
+  :ensure t 
+  :config
+(which-key-mode))
+
+(use-package which-key
+  :ensure t 
+  :config
+(which-key-mode))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package company
+:ensure t)
+
+(use-package corfu
+ ;; Optional customizations
+:custom
+(corfu-cyclt)                ;; Enable cycling for `corfu-next/previous'
+
+(corfu-auto t)                 ;; Enable auto completion
+ ;; (corfu-separator ?\s)          ;; Orderless field separator
+ ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+ ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+ ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+ ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+ ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+ ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+ ;; Enable Corfu only for certain modes.
+ ;; :hook ((prog-mode . corfu-mode)
+ ;;        (shell-mode . corfu-mode)
+ ;;        (eshell-mode . corfu-mode))
+
+ ;; Recommended: Enable Corfu globally.
+ ;; This is recommended since Dabbrev can be used globally (M-/).
+ ;; See also `corfu-exclude-modes'.
+ :init
+ (global-corfu-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :ensure t
+  :config
+  (setq lsp-keymap-prefix "C-c l")
+  (lsp-enable-which-key-integration t))
+
+(use-package eglot
+  :ensure t
+  :hook
+  ((rustic-mode-hook . eglot-ensure)))
+
+(use-package rustic
+:ensure t
+:config
+(setq lsp-rust-analyzer-completion-add-call-parenthesis nil))
